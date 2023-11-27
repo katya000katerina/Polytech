21180216c67da44802a4189213fc70dcad9dceef
Katerina <krasnvasilk@gmail.com>
2023-11-27 20:32:58 +0300

    Modify for j120_1_4

--- src/main/java/org/j110/lab2/university/people/Student.java
+++ src/main/java/org/j110/lab2/university/people/Student.java
@@ -4,6 +4,8 @@ import org.j110.lab2.university.enums.Gender;
 import org.j110.lab2.university.enums.StudyLevel;
 import org.j110.lab2.university.enums.YearOfStudy;
 
+import java.util.Collection;
+
 public class Student extends Person{
     private StudyLevel studyLevel;
     private YearOfStudy yearOfStudy;
@@ -44,5 +46,14 @@ public class Student extends Person{
                 String.format(" %s studies at %s. %1$s is a %s year %s student.",
                         super.getGender().getSubjectPronoun(), super.getDepartment(), yearOfStudy.getOrdinalNumber(),
                         studyLevel.name().toLowerCase());
+    }
+
+    public static void fillCollection(Collection<? super Student> students){
+        if (students == null){
+            throw new IllegalArgumentException("Collection is not initialized");
+        }
+        students.add(new Student("Leo Wilkinson", Gender.MALE, "Computer science", StudyLevel.BACHELOR, YearOfStudy.III));
+        students.add(new Student("Anna Cunningham", Gender.FEMALE, "World economy", StudyLevel.BACHELOR, YearOfStudy.I));
+        students.add(new Student("Jill Lundqvist", Gender.FEMALE, "Jurisprudence", StudyLevel.MASTER, YearOfStudy.I));
     }
 }
